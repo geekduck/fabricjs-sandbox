8fc3ce33bfe53d5e880af5e3e4d44f617acbf35f
geekduck <duck1218+github@gmail.com>
2018-03-09 11:55:11 +0900

    グループ化したオブジェクトで特定のオブジェクトをクリックしたらグループ化を解除してそのオブジェクトだけ動くようにした

--- js/main.js
+++ js/main.js
@@ -1,79 +1,90 @@
 window.onload = () => {
-  const canvas = new fabric.Canvas('c');
 
-  function createImageObject(url) {
-    return new Promise((resolve, reject) => {
-      fabric.Image.fromURL(url, function(img) {
-        console.dir(arguments);
-        if (img) {
-          img.set({
-            left: 10,
-            top: 30
-          });
-          resolve(img);
-        } else {
-          reject();
+    function monkeyPatch() {
+        if (fabric.Canvas.prototype._original_discardActiveObject == null) {
+            fabric.Canvas.prototype._original_discardActiveObject = fabric.Canvas.prototype._discardActiveObject;
+            fabric.Canvas.prototype._discardActiveObject = function() {
+                this._currentTransform = null; // このプロパティが残っているとグループ上のrectをクリックした後にマウスを動かしても反応しないため消す
+                return fabric.Canvas.prototype._original_discardActiveObject.apply(this, arguments);
+            };
         }
-      });
-    });
-  }
+    }
+    monkeyPatch();
 
-  function createRect(props) {
-    return new fabric.Rect({
-      fill: 'black',
-      left: props.x,
-      top: props.y,
-      width: 20,
-      height: 20,
-      hasControls: false
-    });
-  }
+    const canvas = new fabric.Canvas('c');
 
-  const rect1 = createRect({x: 50, y: 50});
-  const rect2 = createRect({x: 100, y: 50});
+    function createImageObject(url) {
+        return new Promise((resolve, reject) => {
+            fabric.Image.fromURL(url, function(img) {
+            console.dir(arguments);
+                if (img) {
+                    img.set({left: 10, top: 30});
+                    resolve(img);
+                } else {
+                    reject();
+                }
+            });
+        });
+    }
 
-  const imageUrl = 'assets/image1.png';
-  createImageObject(imageUrl).then((img) => {
-    canvas.add(img);
+    function createRect(props) {
+        return new fabric.Rect({
+            fill: 'black',
+            left: props.x,
+            top: props.y,
+            width: 20,
+            height: 20,
+            hasControls: false
+        });
+    }
+
+    const rect1 = createRect({x: 50, y: 50});
+    const rect2 = createRect({x: 100, y: 50});
+
+    const imageUrl = 'assets/image1.png';
+    createImageObject(imageUrl).then((img) => {
+        canvas.add(img);
     canvas.add(rect1);
     canvas.add(rect2);
 
     img.on("selected", (e) => {
-      canvas.discardActiveObject();
-      const objects = [img, rect1, rect2];
-      const group = new fabric.Group(objects, {subTargetCheck: true});
-      objects.forEach(object => canvas.remove(object));
-      canvas.add(group);
-      canvas.__onMouseDown(e.e);
+        canvas.discardActiveObject();
+    const objects = [img, rect1, rect2];
+    const group = new fabric.Group(objects, {subTargetCheck: true});
+    objects.forEach(object => canvas.remove(object));
+    canvas.add(group);
+    canvas.__onMouseDown(e.e);
 
-      group.on('deselected', () => {
-          destroyGroup(group);
-      });
+    group.on('deselected', () => {
+        destroyGroup(group);
+});
 
-      group.on('mousedown', (options) => {
-          console.dir(options);
-          if (options.subTargets.filter((object) => object.get("type") === "image").length === 0) {
-              console.log("マップ以外をクリック");
-              const target = options.subTargets.filter((object) => object.get("type") !== "image")[0];
-              destroyGroup(options.target);
-              console.dir(target);
-              canvas.setActiveObject(target);
-              canvas.__onMouseDown(options.e);
-          } else {
-              console.log("マップをクリック");
-          }
-      });
-    });
-  }).catch((error) => console.log("error"));
+    group.on('mousedown', (options) => {
+        if (options.subTargets.filter((object) => object.get("type") === "image").length === 0) {
+        console.log("Image以外をクリック");
+        const target = options.subTargets.filter((object) => object.get("type") !== "image")[0];
+        if (target == null) {
+            // コントロールとかスケールをクリックしたとき
+            return;
+        }
+        destroyGroup(options.target);
+        canvas.setActiveObject(target);
+        canvas.__onMouseDown(options.e);
+    } else {
+        console.log("Imageをクリック");
+    }
+});
+});
+}).catch((error) => console.log("error"));
 
-  function destroyGroup(group) {
-      console.log('destroyGroup');
-      if (group == null) return;
+    function destroyGroup(group) {
+        if (group == null)
+            return;
 
-      const canvas = group.canvas;
-      group.off();
-      group.getObjects().forEach((object) => canvas.add(object));
-      group.destroy();
-      canvas.remove(group);
-  }
+        const canvas = group.canvas;
+        group.off();
+        group.getObjects().forEach((object) => canvas.add(object));
+        group.destroy();
+        canvas.remove(group);
+    }
 };
